f9281bea95c9cfac3f4e84952eab4afb952b61e4
Fredrik Olsson <peylow@gmail.com>
2010-12-28 23:07:26 +0100

    Removed unused code

--- main.m
+++ main.m
@@ -8,30 +8,9 @@
 
 #import <UIKit/UIKit.h>
 
-#if !TARGET_OS_IPHONE || TARGET_IPHONE_SIMULATOR
-#define EXTENDED_DEBUG_SUPPORTED
-#else
-#undef EXTENDED_DEBUG_SUPPORTED
-#endif
-
-#ifdef EXTENDED_DEBUG_SUPPORTED
-#import "GTMStackTrace.h"
-
-void exceptionHandler(NSException *exception) {
-	NSLog(@"%@", GTMStackTraceFromException(exception));
-}
-#endif
-
-
 int main(int argc, char *argv[]) {
-
-#ifdef EXTENDED_DEBUG_SUPPORTED
-	NSLog(@"Debug enabled");
-  NSSetUncaughtExceptionHandler(&exceptionHandler);
-#endif
-
-  NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
-  int retVal = UIApplicationMain(argc, argv, nil, nil);
-  [pool release];
-  return retVal;
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
 }
